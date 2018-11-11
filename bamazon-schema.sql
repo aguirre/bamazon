14409ce9809aad1642e2b5d74cd443b69b064c12
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-11 01:01:56 -0800

    update schema file

--- bamazon-schema.sql
+++ bamazon-schema.sql
@@ -11,3 +11,11 @@ CREATE TABLE products(
     stock_quantity INTEGER(11) NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('outdoormaster ski goggles', 'sports', 49.99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('jbl bluetooth speaker', 'music', 79.99, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('adidas busenitz shoes', 'shoes', 69.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('gopro hero7 black', 'home', 399.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('brita water pitcher', 'kitchen', 27.99, 70);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('sheba cat food', 'pets', 13.50, 80);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('cheez-it', 'food', 4.99, 300);
