1e2ce13909b5f87bf9a69564e158af75f5359acb
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-11 18:05:50 -0800

    add manager menu

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,3 +10,46 @@ var connection = mysql.createConnection({
   password: "password",
   database: "bamazon"
 });
+
+// Global Variables
+let currentProducts = [];
+
+// Bamazon Manager View
+let runManager = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "View Products for Sale":
+          viewSales();
+          break;
+
+        case "View Low Inventory":
+          viewLow();
+          break;
+
+        case "Add to Inventory":
+          createArray();
+          break;
+
+        case "Add New Product":
+          newItem();
+          break;
+      }
+    });
+};
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runManager();
+});
