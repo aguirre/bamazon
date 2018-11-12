32845cb83f03457429b004b7130358fbe8f6bd1e
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-11 19:36:09 -0800

    add update inventory functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -83,6 +83,66 @@ var viewLow = () => {
   );
 };
 
+var createArray = () => {
+  connection.query("SELECT product_name FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      currentProducts.push(res[i].product_name);
+    }
+    addInv();
+  });
+};
+
+var addInv = () => {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "list",
+        message: "Which item would you like to add inventory to?",
+        choices: currentProducts
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to add?",
+        validate: function(value) {
+          if (Number.isInteger(parseInt(value)) && value > 0) {
+            return true;
+          } else {
+            return `${value} is not a valid quantity`;
+          }
+        }
+      }
+    ])
+    .then(function(answers) {
+      connection.query(
+        "SELECT product_name, stock_quantity FROM products WHERE ?",
+        { product_name: answers.item },
+        function(err, res) {
+          var product = res[0];
+          var quantity =
+            parseInt(product.stock_quantity) + parseInt(answers.quantity);
+          if (err) throw err;
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              { stock_quantity: quantity },
+              { product_name: product.product_name }
+            ],
+            function(err, res) {
+              if (err) throw err;
+              console.log(
+                `${product.product_name} stock is now updated to ${quantity}`
+              );
+              runManager();
+            }
+          );
+        }
+      );
+    });
+};
+
 connection.connect(function(err) {
   if (err) throw err;
   runManager();
